2f810cec1bb5a6416fc159bb14d583d990fa4456
Denisa Celaj <125696730+2DNica@users.noreply.github.com>
2024-12-12 16:27:07 +0100

    Update connessione.php

--- inc/connessione.php
+++ inc/connessione.php
@@ -1,7 +1,7 @@
 <?php
 
 // connessione del database
-$conn = mysqli_connect(HOST, 'stadw1307_user', 'u@}5Ku6]^zI9', 'stadw1307_db');
+$conn = mysqli_connect(HOST, 'stadw1307_user', '..........', 'stadw1307_db'); // mettere la password del tuo db
 if ($conn == false) {
     die("Errore di connesione" . mysqli_connect_error());
 }
